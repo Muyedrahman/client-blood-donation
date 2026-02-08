7e7d00ed613bf3f444c0f07df34b2bcd0e1a8366
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-09 00:09:11 +0600

    correct DonationRequest component and implement data fetching with axios

--- src/Pages/DonationRequest/DonationRequest.jsx
+++ src/Pages/DonationRequest/DonationRequest.jsx
@@ -4,51 +4,63 @@ import { Link } from 'react-router-dom';
 
 
 const DonationRequest = () => {
-    const [donationRequest, setDonationRequest] = useState([]);
+  const [donationRequest, setDonationRequest] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      const status = "pending";
-      const { data } = await axios.get(
-        `https://blood-for-life.vercel.app/donation-requests/home/${status}`
-      );
-      setDonationRequest(data);
-    })();
+    const fetchData = async () => {
+      try {
+        const status = "pending";
+        const { data } = await axios.get(
+          `https://blood-for-life.vercel.app/donation-requests/home/${status}`,
+        );
+        setDonationRequest(data);
+      } catch (error) {
+        console.error("Error fetching donation requests:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Recipient Name</th>
-            <th>Location</th>
-            <th>Data</th>
-            <th>Time</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {DonationRequest.map((donation, index) => (
-            <tr key={donation?._id} className="bg-base-200">
-              <th>{index + 1}</th>
-              <td>{donation?.recipientName}</td>
-              <td>
-                {donation?.recipientUpazila}, {donation?.recipientDistrict}
-              </td>
-              <td>{donation?.donationDate}</td>
-              <td>{donation?.donationTime}</td>
-              <td>
-                <Link to={`/view-details/${donation?._id}`}>
-                  <button className="btn btn-success">View Details</button>
-                </Link>
-              </td>
+    <div className="my-10 lg:my-20">
+      <h2 className="text-4xl lg:text-5xl font-semibold lg:font-bold text-center mb-4 lg:mb-6">
+        Donation Request Page
+      </h2>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Recipient Name</th>
+              <th>Location</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Details</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {donationRequest.map((donation, index) => (
+              <tr key={donation?._id} className="bg-base-200">
+                <th>{index + 1}</th>
+                <td>{donation?.recipientName}</td>
+                <td>
+                  {donation?.recipientUpazila}, {donation?.recipientDistrict}
+                </td>
+                <td>{donation?.donationDate}</td>
+                <td>{donation?.donationTime}</td>
+                <td>
+                  <Link to={`/view-details/${donation?._id}`}>
+                    <button className="btn btn-success">View Details</button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
