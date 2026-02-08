1e5cac43c327b98205e7c70d43582b2386a707b2
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-09 00:03:25 +0600

    create useDonationRequest hook for fetching pending requests

--- src/Pages/DonationRequest/DonationRequest.jsx
+++ src/Pages/DonationRequest/DonationRequest.jsx
@@ -1,8 +1,20 @@
-import React,  from 'react';
+import axios from 'axios';
+import React,{ useEffect, useState }  from 'react';
 import { Link } from 'react-router-dom';
 
 
 const DonationRequest = () => {
+    const [donationRequest, setDonationRequest] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const status = "pending";
+      const { data } = await axios.get(
+        `https://blood-for-life.vercel.app/donation-requests/home/${status}`
+      );
+      setDonationRequest(data);
+    })();
+  }, []);
 
 
   return (
